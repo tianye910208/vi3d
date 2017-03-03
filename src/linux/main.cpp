7ce23bcba12f95fa72fd999dedef1bc9810934b4
tianye <tianye910208@foxmail.com>
2017-03-03 18:40:08 +0800

    linux

--- src/linux/main.cpp
+++ src/linux/main.cpp
@@ -3,17 +3,21 @@
 #include <string.h>
 #include <stdarg.h>
 #include <sys/time.h>
-#include "esUtil.h"
 
 #include  <X11/Xlib.h>
 #include  <X11/Xatom.h>
 #include  <X11/Xutil.h>
 
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+
 // X11 related local variables
 static Display *x_display = NULL;
 
 EGLNativeDisplayType nativeDisplay = EGL_DEFAULT_DISPLAY;
-EGLNativeWindowType  nativeWindow = NULL;
+EGLNativeWindowType  nativeWindow;
 
 EGLDisplay eglDisplay;
 EGLContext eglContext;
@@ -71,6 +75,14 @@ bool egl_init()
 	return true;
 }
 
+void egl_exit()
+{
+	eglMakeCurrent(eglDisplay, NULL, NULL, NULL);
+	eglDestroyContext(eglDisplay, eglContext);
+	eglDestroySurface(eglDisplay, eglSurface);
+	eglTerminate(eglDisplay);
+}
+
 
 bool win_init(const char *title, int w, int h)
 {
@@ -91,7 +103,7 @@ bool win_init(const char *title, int w, int h)
 	x_display = XOpenDisplay(NULL);
 	if (x_display == NULL)
 	{
-		return EGL_FALSE;
+		return false;
 	}
 
 	root = DefaultRootWindow(x_display);
@@ -99,15 +111,15 @@ bool win_init(const char *title, int w, int h)
 	swa.event_mask = ExposureMask | PointerMotionMask | KeyPressMask;
 	win = XCreateWindow(
 		x_display, root,
-		0, 0, esContext->width, esContext->height, 0,
+		0, 0, w, h, 0,
 		CopyFromParent, InputOutput,
 		CopyFromParent, CWEventMask,
 		&swa);
 
-	xattr.override_redirect = FALSE;
+	xattr.override_redirect = false;
 	XChangeWindowAttributes(x_display, win, CWOverrideRedirect, &xattr);
 
-	hints.input = TRUE;
+	hints.input = true;
 	hints.flags = InputHint;
 	XSetWMHints(x_display, win, &hints);
 
@@ -116,7 +128,7 @@ bool win_init(const char *title, int w, int h)
 	XStoreName(x_display, win, title);
 
 	// get identifiers for the provided atom name strings
-	wm_state = XInternAtom(x_display, "_NET_WM_STATE", FALSE);
+	wm_state = XInternAtom(x_display, "_NET_WM_STATE", false);
 
 	memset(&xev, 0, sizeof(xev));
 	xev.type = ClientMessage;
@@ -124,17 +136,17 @@ bool win_init(const char *title, int w, int h)
 	xev.xclient.message_type = wm_state;
 	xev.xclient.format = 32;
 	xev.xclient.data.l[0] = 1;
-	xev.xclient.data.l[1] = FALSE;
+	xev.xclient.data.l[1] = false;
 	XSendEvent(
 		x_display,
 		DefaultRootWindow(x_display),
-		FALSE,
+		false,
 		SubstructureNotifyMask,
 		&xev);
 
 	nativeWindow = (EGLNativeWindowType)win;
 	nativeDisplay = (EGLNativeDisplayType)x_display;
-	return EGL_TRUE;
+	return true;
 }
 
 ///
@@ -143,11 +155,12 @@ bool win_init(const char *title, int w, int h)
 //      Reads from X11 event loop and interrupt program if there is a keypress, or
 //      window close action.
 //
-GLboolean userInterrupt(ESContext *esContext)
+bool userInterrupt()
 {
 	XEvent xev;
 	KeySym key;
-	GLboolean userinterrupt = GL_FALSE;
+
+	bool userinterrupt = false;
 	char text;
 
 	// Pump all messages from X server. Keypresses are directed to keyfunc (if defined)
@@ -158,12 +171,11 @@ GLboolean userInterrupt(ESContext *esContext)
 		{
 			if (XLookupString(&xev.xkey, &text, 1, &key, 0) == 1)
 			{
-				if (esContext->keyFunc != NULL)
-					esContext->keyFunc(esContext, text, 0, 0);
+				//prinrf(text);
 			}
 		}
 		if (xev.type == DestroyNotify)
-			userinterrupt = GL_TRUE;
+			userinterrupt = true;
 	}
 	return userinterrupt;
 }
@@ -173,7 +185,7 @@ GLboolean userInterrupt(ESContext *esContext)
 //
 //      Start main windows loop
 //
-void WinLoop(ESContext *esContext)
+void win_loop()
 {
 	struct timeval t1, t2;
 	struct timezone tz;
@@ -181,7 +193,7 @@ void WinLoop(ESContext *esContext)
 
 	gettimeofday(&t1, &tz);
 
-	while (userInterrupt(esContext) == GL_FALSE)
+	while (userInterrupt() == false)
 	{
 		gettimeofday(&t2, &tz);
 		deltatime = (float)(t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec) * 1e-6);
@@ -190,7 +202,7 @@ void WinLoop(ESContext *esContext)
 		//update
 		//render
 
-		eglSwapBuffers(eglDisplay, eglSurface);
+		//eglSwapBuffers(eglDisplay, eglSurface);
 	}
 }
 
