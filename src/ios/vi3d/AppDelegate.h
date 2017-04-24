07c0d0f5bd259ff046779d945c8363b1713f67a7
tianye <tianye@tymac.lan>
2017-04-24 18:51:40 +0800

    iosproject

---
+++ src/ios/vi3d/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vi3d
+//
+//  Created by tianye on 2017/4/21.
+//  Copyright © 2017年 viware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/ios/vi3d/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  vi3d
+//
+//  Created by tianye on 2017/4/21.
+//  Copyright © 2017年 viware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
