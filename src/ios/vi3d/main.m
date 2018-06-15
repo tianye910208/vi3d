c40b3a587f958225cf3b814fb8105bb12a7182d4
Tianye <tianye910208@foxmail.com>
2018-06-15 14:08:00 +0800

    agl

--- src/ios/vi3d/main.m
+++ src/ios/vi3d/main.m
@@ -19,69 +19,11 @@ struct timeval t1, t2;
 vi_msg* msg = NULL;
 
 
-CAEAGLLayer* eaglLayer = NULL;
-EAGLContext* eaglContext = NULL;
-
-
 @interface OpenGLView : UIView
 
 @end
 
-
 @implementation OpenGLView
-
-- (id)initWithFrame:(CGRect)frame {
-	self = [super initWithFrame:frame];
-
-	eaglLayer = (CAEAGLLayer*) self.layer;
-	eaglLayer.opaque = YES;
-		
-	eaglContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
-	if (!eaglContext) {
-		NSLog(@"[VI3D]Failed to get OpenGLES context");
-		exit(1);
-	}
-	
-	if (![EAGLContext setCurrentContext:eaglContext]) {
-		NSLog(@"[VI3D]Failed to set OpenGLES context");
-		exit(1);
-	}
-
-    GLuint depthRenderBuffer;
-    glGenRenderbuffers(1, &depthRenderBuffer);
-    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, self.frame.size.width, self.frame.size.height);
-
-    GLuint colorRenderBuffer;
-    glGenRenderbuffers(1, &colorRenderBuffer);
-	glBindRenderbuffer(GL_RENDERBUFFER, colorRenderBuffer);
-	[eaglContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:eaglLayer];
-
-    
-    GLuint framebuffer;
-	glGenFramebuffers(1, &framebuffer);
-	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRenderBuffer);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer);
-
-
-    gettimeofday(&t1, NULL);
-    CADisplayLink* displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render:)];
-	[displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    
-	return self;
-}
-
-- (void)render:(CADisplayLink*)displayLink {
-    gettimeofday(&t2, NULL);
-    float dt = (float)(t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec) * 1e-6);
-    t1 = t2;
-    if(actived && eaglContext) {
-        vi_app_loop(dt);
-        [eaglContext presentRenderbuffer:GL_RENDERBUFFER];
-    }
-}
 
 + (Class)layerClass {
 	return [CAEAGLLayer class];
@@ -157,9 +99,13 @@ EAGLContext* eaglContext = NULL;
 
     self.window = [[UIWindow alloc] initWithFrame:rect];
     [self.window  setScreen:UIScreen.mainScreen];
+
+
+    OpenGLView* eglview = [[OpenGLView alloc] initWithFrame:[self.window bounds]];
+    eglview.layer.opaque = YES;
 	
     UIViewController* viewctl = [[UIViewController alloc] init];
-	[viewctl setView:[[OpenGLView alloc] initWithFrame:[self.window bounds]]];
+	[viewctl setView:eglview];
     [viewctl prefersStatusBarHidden];
 
     [self.window setRootViewController:viewctl];
@@ -173,12 +119,25 @@ EAGLContext* eaglContext = NULL;
 
     vi_app_set_screen_size(rect.size.width, rect.size.height);
 
-    if(vi_app_init(datadir, savedir)) return NO;
+    if(vi_app_init(eglview.layer, eglview.frame.size, datadir, savedir)) return NO;
 	if(vi_app_main()) return NO;
 
     actived = 1;
+
+    gettimeofday(&t1, NULL);
+    CADisplayLink* displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(mainloop:)];
+	[displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
     
     return YES;
+}
+
+
+- (void)mainloop:(CADisplayLink*)displayLink {
+    gettimeofday(&t2, NULL);
+    float dt = (float)(t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec) * 1e-6);
+    t1 = t2;
+    if(actived && eaglContext)
+        vi_app_loop(dt);
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
--- src/vi_gles.c
+++ src/vi_gles.c
@@ -96,7 +96,42 @@ int vi_gles_egl_swap() {
 
 #ifdef VI3D_GLES_AGL
 
-int vi_gles_agl_init() {
+CAEAGLLayer* eaglLayer = NULL;
+EAGLContext* eaglContext = NULL;
+
+int vi_gles_agl_init(CAEAGLLayer* layer) {
+	eaglLayer = layer;
+		
+	eaglContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+	if (!eaglContext) {
+		vi_log("[E]Failed to get OpenGLES context");
+		return 101;
+	}
+	
+	if (![EAGLContext setCurrentContext:eaglContext]) {
+		vi_log("[E]Failed to set OpenGLES context");
+		return 102;
+	}
+
+    GLuint depthRenderBuffer;
+    glGenRenderbuffers(1, &depthRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, self.frame.size.width, self.frame.size.height);
+
+    GLuint colorRenderBuffer;
+    glGenRenderbuffers(1, &colorRenderBuffer);
+	glBindRenderbuffer(GL_RENDERBUFFER, colorRenderBuffer);
+	[eaglContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:eaglLayer];
+
+    
+    GLuint framebuffer;
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRenderBuffer);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer);
+
+
 	return 0;
 }
 
@@ -105,6 +140,7 @@ int vi_gles_agl_exit() {
 }
 
 int vi_gles_agl_swap() {
+	[eaglContext presentRenderbuffer:GL_RENDERBUFFER];
 	return 0;
 }
 
